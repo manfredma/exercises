25c27065b866635168f3a11ffe1b4159fbe97990
maxingfang <maxingfang@jd.com>
2019-05-21 18:07:37 +0800

    add leetcode 22

---
+++ do-exercise/leet-code/src/main/java/exe22/generate/parentheses/Main.java
@@ -0,0 +1,21 @@
+/*
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+ */
+
+package exe22.generate.parentheses;
+
+/**
+ * @author Manfred since 2019/5/21
+ */
+public class Main {
+}
---
+++ do-exercise/leet-code/src/main/java/exe22/generate/parentheses/Solution.java
@@ -0,0 +1,11 @@
+package exe22.generate.parentheses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        return result;
+    }
+}
