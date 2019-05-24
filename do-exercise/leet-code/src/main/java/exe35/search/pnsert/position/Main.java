3f8dc6fd756ecfd4a009caa8579269a99f752917
maxf <maxf@startimes.com.cn>
2019-05-25 00:43:15 +0800

    add leetcode 35

--- do-exercise/leet-code/src/main/java/exe35/search/pnsert/position/Main.java
+++ do-exercise/leet-code/src/main/java/exe35/search/pnsert/position/Main.java
@@ -28,4 +28,11 @@ package exe35.search.pnsert.position;
  * @author Manfred since 2019/5/24
  */
 public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.searchInsert(new int[] {1,3,5,6}, 7));
+        System.out.println(solution.searchInsert(new int[] {1,3,5,6}, 5));
+        System.out.println(solution.searchInsert(new int[] {1,3,5,6}, 2));
+        System.out.println(solution.searchInsert(new int[] {1,3,5,6}, 0));
+    }
 }
--- do-exercise/leet-code/src/main/java/exe35/search/pnsert/position/Solution.java
+++ do-exercise/leet-code/src/main/java/exe35/search/pnsert/position/Solution.java
@@ -2,6 +2,42 @@ package exe35.search.pnsert.position;
 
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        return 0;
+        int result = 0;
+        if (null == nums || nums.length == 0) {
+            return result;
+        }
+
+
+
+        return search(nums, target, 0, nums.length - 1);
     }
+
+    private int search(int[] nums, int target, int leftI, int rightI) {
+        while (true) {
+            if (leftI >= 0 && leftI <= nums.length - 1 && nums[leftI] >= target) {
+                return leftI;
+            }
+            if (rightI >= 0 && rightI <= nums.length - 1 && nums[rightI] <= target) {
+
+                return nums[rightI] == target? rightI:rightI + 1;
+            }
+            if (leftI >= rightI) {
+                if (target < nums[rightI]) {
+                    return rightI;
+                } else {
+                    return leftI;
+                }
+            }
+
+            int m = leftI + (rightI - leftI) / 2;
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[m] < target) {
+                return search(nums, target, m + 1, rightI - 1);
+            } else {
+                return search(nums, target, leftI + 1, m - 1);
+            }
+        }
+    }
+
 }
