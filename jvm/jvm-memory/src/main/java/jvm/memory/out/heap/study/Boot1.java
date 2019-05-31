f5e23b61d6e6899b1d8c8b3723e7a235d83602bf
maxingfang <maxingfang@jd.com>
2019-06-01 02:53:10 +0800

    todo

--- jvm/jvm-memory/src/main/java/jvm/memory/out/heap/study/Boot1.java
+++ jvm/jvm-memory/src/main/java/jvm/memory/out/heap/study/Boot1.java
@@ -1,7 +1,16 @@
 package jvm.memory.out.heap.study;
 
+import java.nio.ByteBuffer;
+
 /**
  * @author Manfred since 2019/5/31
  */
 public class Boot1 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (true) {
+            System.out.println("第" + (i++) + "次");
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024 * 1024);
+        }
+    }
 }
