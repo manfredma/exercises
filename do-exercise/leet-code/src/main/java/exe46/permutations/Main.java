f02e9e8c1011eb92d048d2f03ffd119b07a4c015
maxingfang <maxingfang@jd.com>
2019-07-03 19:26:55 +0800

    add leetcode 46

---
+++ do-exercise/leet-code/src/main/java/exe46/permutations/Main.java
@@ -0,0 +1,28 @@
+/*
+Given a collection of distinct integers, return all possible permutations.
+
+Example:
+
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+ */
+
+package exe46.permutations;
+
+/**
+ * @author Manfred since 2019/7/3
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.permute(new int[]{1, 2, 3}));
+    }
+}
