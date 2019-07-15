5d31e7ef79c6efe26c1c2daca56ba5fe5264a68c
maxingfang <maxingfang@jd.com>
2019-07-15 11:24:19 +0800

    add leetcode 51

--- do-exercise/leet-code(51-100)/src/main/java/exe51/n/queens/Main.java
+++ do-exercise/leet-code(51-100)/src/main/java/exe51/n/queens/Main.java
@@ -5,7 +5,8 @@ The n-queens puzzle is the problem of placing n queens on an n×n chessboard suc
 
 Given an integer n, return all distinct solutions to the n-queens puzzle.
 
-Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
+Each solution contains a distinct board configuration of the n-queens' placement,
+where 'Q' and '.' both indicate a queen and an empty space respectively.
 
 Example:
 
@@ -29,4 +30,8 @@ package exe51.n.queens;
  * @author Manfred since 2019/7/4
  */
 public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solveNQueens(4));
+    }
 }
