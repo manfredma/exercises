fae590bae76bbfe2737b63706bfd845ba57d2fb1
manfred <maxingfang@126.com>
2019-07-17 21:16:00 +0800

    add leetcode 77

--- do-exercise/leet-code(51-100)/src/main/java/exe77/combinations/Solution.java
+++ do-exercise/leet-code(51-100)/src/main/java/exe77/combinations/Solution.java
@@ -32,20 +32,22 @@ class Solution {
             }
             result.add(integers);
             return result;
+        } else if (k == 1) {
+            for (int i = 0; i < end - begin + 1; i++) {
+                List<Integer> re = new ArrayList<>();
+                re.add(digits[begin + i]);
+                result.add(re);
+            }
+            return result;
+        }
+        List<List<Integer>> x = doCombine(digits, k - 1, begin + 1, end);
+        for (List<Integer> integers : x) {
+            List<Integer> integers1 = new ArrayList<>();
+            integers1.add(digits[begin]);
+            integers1.addAll(integers);
+            result.add(integers1);
         }
         result.addAll(doCombine(digits, k, begin + 1, end));
-        if (k > 1) {
-            List<List<Integer>> x = doCombine(digits, k - 1, begin + 1, end);
-
-            for (List<Integer> integers : x) {
-                List<Integer> integers1 = new ArrayList<>();
-                integers1.add(digits[begin]);
-                integers1.addAll(integers);
-                result.add(integers1);
-            }
-
-
-        }
         return result;
     }
 }
