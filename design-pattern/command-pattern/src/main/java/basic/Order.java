2ddb0be85a79159c9c65b472404e369e738edcb5
maxingfang <maxingfang@jd.com>
2019-08-15 16:45:59 +0800

    command pattern

---
+++ design-pattern/command-pattern/src/main/java/basic/Order.java
@@ -0,0 +1,5 @@
+package basic;
+
+public interface Order {
+   void execute();
+}
---
+++ design-pattern/command-pattern/src/main/java/basic/SellStock.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class SellStock implements Order {
+   private Stock abcStock;
+ 
+   public SellStock(Stock abcStock){
+      this.abcStock = abcStock;
+   }
+ 
+   public void execute() {
+      abcStock.sell();
+   }
+}
