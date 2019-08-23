d1df3de176daf0bde3f54a434e9e3633cec61aa8
manfred <maxingfang@126.com>
2019-08-23 22:07:59 +0800

    add leetcode 88

---
+++ do-exercise/leet-code(51-100)/src/main/java/exe88/merge/sorted/array/Solution.java
@@ -0,0 +1,25 @@
+package exe88.merge.sorted.array;
+
+/**
+ * @author manfred
+ */
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[i + j + 1] = nums1[i];
+                i--;
+            } else {
+                nums1[i + j + 1] = nums2[j];
+                j--;
+            }
+        }
+
+        if (j >= 0) {
+            for (int k = 0; k <= j; k++) {
+                nums1[k] = nums2[k];
+            }
+        }
+    }
+}
