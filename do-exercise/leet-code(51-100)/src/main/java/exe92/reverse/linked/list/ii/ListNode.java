173f6a85ee227ba0f39e952160f0eb5d883627ff
manfred <maxingfang@126.com>
2019-08-25 14:26:59 +0800

    add leetcode 92

---
+++ do-exercise/leet-code(51-100)/src/main/java/exe92/reverse/linked/list/ii/ListNode.java
@@ -0,0 +1,15 @@
+package exe92.reverse.linked.list.ii;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(val);
+    }
+}
