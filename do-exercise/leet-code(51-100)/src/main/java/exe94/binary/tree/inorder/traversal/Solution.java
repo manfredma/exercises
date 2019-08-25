5c3fb19eb451918e449861451798562b9fb1858f
manfred <maxingfang@126.com>
2019-08-25 16:23:11 +0800

    add leetcode 94

---
+++ do-exercise/leet-code(51-100)/src/main/java/exe94/binary/tree/inorder/traversal/Solution.java
@@ -0,0 +1,34 @@
+package exe94.binary.tree.inorder.traversal;
+
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (null == root) {
+            return result;
+        }
+
+        doInorderTraversal(root, result);
+        return result;
+    }
+
+    private void doInorderTraversal(TreeNode node, List<Integer> result) {
+        if (null != node) {
+            doInorderTraversal(node.left, result);
+            result.add(node.val);
+            doInorderTraversal(node.right, result);
+        }
+    }
+}
---
+++ do-exercise/leet-code(51-100)/src/main/java/exe94/binary/tree/inorder/traversal/TreeNode.java
@@ -0,0 +1,11 @@
+package exe94.binary.tree.inorder.traversal;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
