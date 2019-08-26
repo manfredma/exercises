7536c672115b751539c25e0aa1ef7b3ad1c96143
maxingfang <maxingfang@jd.com>
2019-08-26 09:30:35 +0800

    add leetcode 102

---
+++ do-exercise/leet-code(101-150)/src/main/java/exe102/binary/tree/level/order/traversal/Main.java
@@ -0,0 +1,41 @@
+/*
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+
+ */
+package exe102.binary.tree.level.order.traversal;
+
+/**
+ * @author Manfred since 2019/8/26
+ */
+public class Main {
+    public static void main(String[] args) {
+        TreeNode treeNode1 = new TreeNode(3);
+        TreeNode treeNode2 = new TreeNode(9);
+        TreeNode treeNode3 = new TreeNode(20);
+        TreeNode treeNode4 = new TreeNode(15);
+        TreeNode treeNode5 = new TreeNode(7);
+
+        treeNode1.left = treeNode2;
+        treeNode1.right = treeNode3;
+
+        treeNode3.left = treeNode4;
+        treeNode3.right = treeNode5;
+
+        System.out.println(new Solution().levelOrder(treeNode1));
+
+    }
+}
