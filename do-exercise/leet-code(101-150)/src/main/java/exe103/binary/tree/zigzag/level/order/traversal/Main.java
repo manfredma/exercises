bb31a2358ddbd4480c851749ff8317d536542352
maxingfang <maxingfang@jd.com>
2019-08-26 10:14:31 +0800

    add leetcode 103

---
+++ do-exercise/leet-code(101-150)/src/main/java/exe103/binary/tree/zigzag/level/order/traversal/Main.java
@@ -0,0 +1,40 @@
+/*
+Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its zigzag level order traversal as:
+[
+  [3],
+  [20,9],
+  [15,7]
+]
+
+ */
+package exe103.binary.tree.zigzag.level.order.traversal;
+
+/**
+ * @author Manfred since 2019/8/26
+ */
+public class Main {
+    public static void main(String[] args) {
+        TreeNode treeNode1 = new TreeNode(3);
+        TreeNode treeNode2 = new TreeNode(9);
+        TreeNode treeNode3 = new TreeNode(20);
+        TreeNode treeNode4 = new TreeNode(15);
+        TreeNode treeNode5 = new TreeNode(7);
+
+        treeNode1.left = treeNode2;
+        treeNode1.right = treeNode3;
+
+        treeNode3.left = treeNode4;
+        treeNode3.right = treeNode5;
+
+        System.out.println(new Solution().zigzagLevelOrder(treeNode1));
+    }
+}
---
+++ do-exercise/leet-code(101-150)/src/main/java/exe103/binary/tree/zigzag/level/order/traversal/TreeNode.java
@@ -0,0 +1,11 @@
+package exe103.binary.tree.zigzag.level.order.traversal;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
