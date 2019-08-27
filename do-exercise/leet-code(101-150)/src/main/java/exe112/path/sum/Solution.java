7183e4102c343b0e5837f38b194820e41f04699c
manfred <maxingfang@126.com>
2019-08-27 22:52:25 +0800

    add leetcode 112

---
+++ do-exercise/leet-code(101-150)/src/main/java/exe112/path/sum/Solution.java
@@ -0,0 +1,38 @@
+package exe112.path.sum;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (null == root) {
+            return false;
+        }
+
+        if (root.left == null && root.right == null) {
+            if (root.val == sum) {
+                return true;
+            }
+        }
+
+        if (root.left != null) {
+            root.left.val = root.val + root.left.val;
+            if (hasPathSum(root.left, sum)) {
+                return true;
+            }
+        }
+        if (root.right != null) {
+            root.right.val = root.val + root.right.val;
+            if (hasPathSum(root.right, sum)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
