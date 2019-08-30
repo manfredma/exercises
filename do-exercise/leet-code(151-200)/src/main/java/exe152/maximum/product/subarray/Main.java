0f4b73615f1ec9248807bec72688039ee5c23673
maxingfang <maxingfang@jd.com>
2019-08-30 13:54:35 +0800

    add leetcode 152 -- 初始化提交

---
+++ do-exercise/leet-code(151-200)/src/main/java/exe152/maximum/product/subarray/Main.java
@@ -0,0 +1,23 @@
+/*
+Given an integer array nums, find the contiguous subarray within an array (containing at least one number)
+which has the largest product.
+
+Example 1:
+
+Input: [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+Example 2:
+
+Input: [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+
+ */
+package exe152.maximum.product.subarray;
+
+/**
+ * @author Manfred since 2019/8/30
+ */
+public class Main {
+}
---
+++ do-exercise/leet-code(151-200)/src/main/java/exe152/maximum/product/subarray/Solution.java
@@ -0,0 +1,7 @@
+package exe152.maximum.product.subarray;
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        return -1;
+    }
+}
