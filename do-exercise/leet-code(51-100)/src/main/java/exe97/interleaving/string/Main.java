e5b58b475307659a8930ee72bd0ebb695753733f
maxingfang <maxingfang@jd.com>
2019-09-02 18:42:40 +0800

    add leetcode 97 -- 提交递归版本

--- do-exercise/leet-code(51-100)/src/main/java/exe97/interleaving/string/Main.java
+++ do-exercise/leet-code(51-100)/src/main/java/exe97/interleaving/string/Main.java
@@ -16,4 +16,9 @@ package exe97.interleaving.string;
  * @author manfred on 2019/8/25.
  */
 public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isInterleave("aabcc", "dbbca", "aadbbcbcac"));
+        System.out.println(solution.isInterleave("aabcc", "dbbca", "aadbbbaccc"));
+    }
 }
--- do-exercise/leet-code(51-100)/src/main/java/exe97/interleaving/string/Solution.java
+++ do-exercise/leet-code(51-100)/src/main/java/exe97/interleaving/string/Solution.java
@@ -2,6 +2,22 @@ package exe97.interleaving.string;
 
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        return false;
+        if ("".equals(s1)) {
+            return s2.equals(s3);
+        } else if ("".equals(s2)) {
+            return s1.equals(s3);
+        } else {
+            if (s1.charAt(0) == s3.charAt(0)) {
+                boolean subIsInterleave1 = isInterleave(s1.substring(1), s2, s3.substring(1));
+                if (subIsInterleave1) {
+                    return true;
+                }
+            }
+            if (s2.charAt(0) == s3.charAt(0)) {
+                return isInterleave(s1, s2.substring(1), s3.substring(1));
+            } else {
+                return false;
+            }
+        }
     }
 }
