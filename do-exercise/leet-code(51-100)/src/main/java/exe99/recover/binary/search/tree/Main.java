ef4b939b0f71dde7e81da6ca16bbaa9c3222078f
manfred <maxingfang@126.com>
2019-09-03 11:43:44 +0800

    add leetcode 99

--- do-exercise/leet-code(51-100)/src/main/java/exe99/recover/binary/search/tree/Main.java
+++ do-exercise/leet-code(51-100)/src/main/java/exe99/recover/binary/search/tree/Main.java
@@ -49,4 +49,32 @@ package exe99.recover.binary.search.tree;
  * @author manfred on 2019/8/25.
  */
 public class Main {
+    public static void main(String[] args) {
+        TreeNode treeNode11 = new TreeNode(1);
+        TreeNode treeNode12 = new TreeNode(2);
+        TreeNode treeNode13 = new TreeNode(3);
+
+        treeNode11.left = treeNode13;
+        treeNode13.right = treeNode12;
+
+        new Solution().recoverTree(treeNode11);
+        System.out.println(treeNode11.val);
+        System.out.println(treeNode12.val);
+        System.out.println(treeNode13.val);
+
+        TreeNode treeNode21 = new TreeNode(1);
+        TreeNode treeNode22 = new TreeNode(2);
+        TreeNode treeNode23 = new TreeNode(3);
+        TreeNode treeNode24 = new TreeNode(4);
+
+        treeNode23.left = treeNode21;
+        treeNode23.right = treeNode24;
+        treeNode24.left = treeNode22;
+
+        new Solution().recoverTree(treeNode23);
+        System.out.println(treeNode21.val);
+        System.out.println(treeNode22.val);
+        System.out.println(treeNode23.val);
+        System.out.println(treeNode24.val);
+    }
 }
