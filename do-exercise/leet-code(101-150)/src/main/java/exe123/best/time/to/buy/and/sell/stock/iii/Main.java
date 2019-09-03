d9e9b3b03e11a42c0bfd556a09bccfac44dbddde
manfred <maxingfang@126.com>
2019-09-03 18:46:36 +0800

    add leetcode 123 动态规划求解

--- do-exercise/leet-code(101-150)/src/main/java/exe123/best/time/to/buy/and/sell/stock/iii/Main.java
+++ do-exercise/leet-code(101-150)/src/main/java/exe123/best/time/to/buy/and/sell/stock/iii/Main.java
@@ -33,8 +33,8 @@ package exe123.best.time.to.buy.and.sell.stock.iii;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.maxProfit(new int[]{3, 3, 5, 0, 0, 3, 1, 4}));
         System.out.println(solution.maxProfit(new int[]{1, 2, 3, 4, 5}));
+        System.out.println(solution.maxProfit(new int[]{3, 3, 5, 0, 0, 3, 1, 4}));
         System.out.println(solution.maxProfit(new int[]{7, 6, 4, 3, 1}));
     }
 }
