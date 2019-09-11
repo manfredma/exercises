3766d33f5d0c394df0067b54a394f57eca61aeb3
manfred <maxingfang@126.com>
2019-09-11 23:11:22 +0800

    临时提交 - 序列化

---
+++ java-serialization/jdk-serialization/src/main/java/jdk/serialization/Main.java
@@ -0,0 +1,7 @@
+package jdk.serialization;
+
+/**
+ * @author manfred on 2019/9/11.
+ */
+public class Main {
+}
---
+++ java-serialization/jdk-serialization/src/main/java/jdk/serialization/UserInfo.java
@@ -0,0 +1,10 @@
+package jdk.serialization;
+
+import java.io.Serializable;
+
+/**
+ * @author manfred on 2019/9/11.
+ */
+public class UserInfo implements Serializable {
+
+}
