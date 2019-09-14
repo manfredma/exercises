2c86c0af30d20f22bf509b1d8f6b063cdcffc15d
manfred <maxingfang@126.com>
2019-09-14 13:40:09 +0800

    netty : add marshalling example

---
+++ netty/netty-serialization/src/main/java/marshalling/MarshallingCodeCFactory.java
@@ -0,0 +1,30 @@
+package marshalling;
+
+import io.netty.handler.codec.marshalling.*;
+import org.jboss.marshalling.MarshallerFactory;
+import org.jboss.marshalling.Marshalling;
+import org.jboss.marshalling.MarshallingConfiguration;
+
+/**
+ * @author manfred on 2019/9/14.
+ */
+public final class MarshallingCodeCFactory {
+
+    public static MarshallingDecoder buildMarshallingDecode() {
+        final MarshallerFactory marshallerFactory = Marshalling.getProvidedMarshallerFactory("serial");
+        final MarshallingConfiguration configuration = new MarshallingConfiguration();
+        configuration.setVersion(5);
+        UnmarshallerProvider provider = new DefaultUnmarshallerProvider(marshallerFactory, configuration);
+        MarshallingDecoder decoder = new MarshallingDecoder(provider, 1024);
+        return decoder;
+    }
+
+    public static MarshallingEncoder buildMarshallingEncode() {
+        final MarshallerFactory marshallerFactory = Marshalling.getProvidedMarshallerFactory("serial");
+        final MarshallingConfiguration configuration = new MarshallingConfiguration();
+        configuration.setVersion(5);
+        MarshallerProvider provider = new DefaultMarshallerProvider(marshallerFactory, configuration);
+        MarshallingEncoder encoder = new MarshallingEncoder(provider);
+        return encoder;
+    }
+}
