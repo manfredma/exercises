b6e27da33c0bc91c0460a6b4c37981ae4ad76ae8
maxingfang <maxingfang@jd.com>
2019-09-17 18:21:38 +0800

    add leetcode 198

---
+++ do-exercise/leet-code(151-200)/src/main/java/exe198/house/robber/Main.java
@@ -0,0 +1,35 @@
+/*
+You are a professional robber planning to rob houses along a street.
+Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them
+is that adjacent houses have security system connected and it will automatically contact the police
+if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house,
+determine the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+             Total amount you can rob = 1 + 3 = 4.
+Example 2:
+
+Input: [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+             Total amount you can rob = 2 + 9 + 1 = 12.
+
+ */
+package exe198.house.robber;
+
+/**
+ * @author Manfred since 2019/9/17
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.rob(new int[]{1, 2, 3, 1}));
+        System.out.println(solution.rob(new int[]{2, 7, 9, 3, 1}));
+    }
+}
