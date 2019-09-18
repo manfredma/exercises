52babfbb517b87add242328d77184cde773c3df5
manfred <maxingfang@126.com>
2019-09-18 20:52:38 +0800

    add leetcode 206

---
+++ do-exercise/leet-code(201-250)/src/main/java/exe206/reverse/linked/list/ListNode.java
@@ -0,0 +1,10 @@
+package exe206.reverse.linked.list;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ do-exercise/leet-code(201-250)/src/main/java/exe206/reverse/linked/list/Main.java
@@ -0,0 +1,43 @@
+/*
+/*
+Reverse a singly linked list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+Follow up:
+
+A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+ */
+
+package exe206.reverse.linked.list;
+
+/**
+ * @author manfred on 2019/9/18.
+ */
+public class Main {
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+        ListNode listNode6 = new ListNode(6);
+
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+        listNode5.next = listNode6;
+
+        Solution solution = new Solution();
+
+        ListNode r = solution.reverseList(listNode1);
+        while (r != null) {
+            System.out.print(r.val + " -> ");
+            r = r.next;
+        }
+    }
+}
