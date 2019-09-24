aeb5accfd4b7a5c2ce2c1a1d5ac08d7e96a543fe
maxingfang <maxingfang@jd.com>
2019-09-24 11:21:55 +0800

    add leetcode 211 - 初始化提交

---
+++ do-exercise/leet-code(201-250)/src/main/java/exe211/add/and/search/word/data/structure/design/Main.java
@@ -0,0 +1,29 @@
+/*
+
+Design a data structure that supports the following two operations:
+
+void addWord(word)
+bool search(word)
+search(word) can search a literal word or a regular expression string containing only letters a-z or .. A . means
+it can represent any one letter.
+
+Example:
+
+addWord("bad")
+addWord("dad")
+addWord("mad")
+search("pad") -> false
+search("bad") -> true
+search(".ad") -> true
+search("b..") -> true
+Note:
+You may assume that all words are consist of lowercase letters a-z.
+
+ */
+package exe211.add.and.search.word.data.structure.design;
+
+/**
+ * @author Manfred since 2019/9/24
+ */
+public class Main {
+}
---
+++ do-exercise/leet-code(201-250)/src/main/java/exe211/add/and/search/word/data/structure/design/WordDictionary.java
@@ -0,0 +1,26 @@
+package exe211.add.and.search.word.data.structure.design;
+
+class WordDictionary {
+
+    /** Initialize your data structure here. */
+    public WordDictionary() {
+        
+    }
+    
+    /** Adds a word into the data structure. */
+    public void addWord(String word) {
+        
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    public boolean search(String word) {
+        return false;
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
