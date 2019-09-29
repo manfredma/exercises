0ff5dd57422d2da3d5a968b5f4c60adae1dca854
maxingfang <maxingfang@jd.com>
2019-09-29 10:34:46 +0800

    netty

--- netty/netty-serialization/src/main/java/message/pack/v1/MsgPackDecoder.java
+++ netty/netty-serialization/src/main/java/message/pack/v1/MsgPackDecoder.java
@@ -4,7 +4,6 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageDecoder;
 import org.msgpack.MessagePack;
-import sun.plugin2.main.client.MessagePassingExecutionContext;
 
 import java.util.List;
 
