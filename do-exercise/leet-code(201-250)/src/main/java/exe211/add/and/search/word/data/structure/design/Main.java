533156caaf5161cb1208121ec33a96fdc8c7cc76
maxingfang <maxingfang@jd.com>
2019-09-29 10:21:17 +0800

    add leetcode 211 - 使用特里树处理

--- do-exercise/leet-code(201-250)/src/main/java/exe211/add/and/search/word/data/structure/design/Main.java
+++ do-exercise/leet-code(201-250)/src/main/java/exe211/add/and/search/word/data/structure/design/Main.java
@@ -26,4 +26,14 @@ package exe211.add.and.search.word.data.structure.design;
  * @author Manfred since 2019/9/24
  */
 public class Main {
+    public static void main(String[] args) {
+        WordDictionary wordDictionary = new WordDictionary();
+        wordDictionary.addWord("bad");
+        wordDictionary.addWord("dad");
+        wordDictionary.addWord("mad");
+        System.out.println(wordDictionary.search("pad"));
+        System.out.println(wordDictionary.search("bad"));
+        System.out.println(wordDictionary.search(".ad"));
+        System.out.println(wordDictionary.search("b.."));
+    }
 }
