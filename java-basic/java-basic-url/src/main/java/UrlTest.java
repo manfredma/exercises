f9e142fe884d64c850fb70b463527d31f34ddb22
maxingfang <maxingfang@didiglobal.com>
2019-11-08 18:16:18 +0800

    【 url 】 test

---
+++ java-basic/java-basic-url/src/main/java/UrlTest.java
@@ -0,0 +1,15 @@
+import java.net.URI;
+
+/**
+ * @author manfred
+ * @since 2019-11-07 上午11:10
+ */
+public class UrlTest {
+    public static void main(String[] args) {
+
+    }
+
+    public static String getQueryString(String url, String tag) {
+       return null;
+    }
+}
