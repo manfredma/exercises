4e0a193bedca235bdc4312148ab9012e3579fdc8
maxingfang <maxingfang@didiglobal.com>
2019-11-12 20:34:54 +0800

    【 number 】 test

---
+++ java-basic/java-basic-other/src/main/java/NumberTest.java
@@ -0,0 +1,10 @@
+/**
+ * @author manfred
+ * @since 2019-11-12 下午8:31
+ */
+public class NumberTest {
+    public static void main(String[] args) {
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
