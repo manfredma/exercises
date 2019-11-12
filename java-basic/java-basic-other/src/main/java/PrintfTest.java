ddd786042cefb0ced52110b51662dac61837b023
maxingfang <maxingfang@didiglobal.com>
2019-11-12 21:08:58 +0800

    【 number 】 test

---
+++ java-basic/java-basic-other/src/main/java/PrintfTest.java
@@ -0,0 +1,12 @@
+import java.io.PrintStream;
+
+/**
+ * @author manfred
+ * @since 2019-11-12 下午8:46
+ */
+public class PrintfTest {
+    public static void main(String[] args) {
+        System.out.printf("%u", 1111111111111111L);
+        System.out.printf("%u", -1111111111111111L);
+    }
+}
