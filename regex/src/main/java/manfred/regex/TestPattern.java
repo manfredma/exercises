8b29827b63b6b2f683f147036af9a659d646ff7e
maxingfang <maxingfang@didiglobal.com>
2019-11-13 19:58:36 +0800

    【 regex 】 test

---
+++ regex/src/main/java/manfred/regex/TestPattern.java
@@ -0,0 +1,21 @@
+package manfred.regex;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+/**
+ * @author manfred
+ * @since 2019-11-13 下午7:51
+ */
+public class TestPattern {
+    public static Pattern pattern = Pattern.compile(".*[a-zA-Z]+.*");
+    public static void main(String[] args) {
+        System.out.println(pattern.matcher("1233a4312").find());
+        System.out.println(pattern.matcher("aa4312").find());
+        System.out.println(pattern.matcher("aa").find());
+        System.out.println(pattern.matcher("132123412").find());
+
+        String x = "1x_23_33";
+        System.out.println(Arrays.toString(x.split("_")));
+    }
+}
