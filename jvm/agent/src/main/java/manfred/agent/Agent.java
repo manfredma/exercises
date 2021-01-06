75ae3cb04b4137a333914c7683f6c7b3718a775e
maxingfang <maxingfang@meituan.com>
2021-01-06 20:12:51 +0800

    jvm-agent init

--- jvm/agent/src/main/java/manfred/agent/Agent.java
+++ jvm/agent/src/main/java/manfred/agent/Agent.java
@@ -4,6 +4,7 @@ import java.lang.instrument.Instrumentation;
 
 public class Agent {
     public static void premain(String args, Instrumentation inst) {
+        new Throwable().printStackTrace();
         System.out.println("Hi, This is a agent!");
         inst.addTransformer(new TestTransformer()); //将类转换器添加到此`agent`的`instrumentation`实例之中
     }
--- jvm/agent/src/main/java/manfred/agent/basic/Boot.java
+++ jvm/agent/src/main/java/manfred/agent/basic/Boot.java
@@ -2,6 +2,7 @@ package manfred.agent.basic;
 
 public class Boot {
     public static void main(String[] args) {
+        new Throwable().printStackTrace();
         System.out.println("hello, world!");
     }
 }
