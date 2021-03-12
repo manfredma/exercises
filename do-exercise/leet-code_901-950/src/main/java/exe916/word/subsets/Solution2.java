13cafebabbddcc4bf7c038fa658658bddaea216e
maxingfang <maxingfang@meituan.com>
2021-03-12 17:13:46 +0800

    done leet-code 916

--- do-exercise/leet-code_901-950/src/main/java/exe916/word/subsets/Solution2.java
+++ do-exercise/leet-code_901-950/src/main/java/exe916/word/subsets/Solution2.java
@@ -4,97 +4,75 @@ import java.util.*;
 
 /**
  * We are given two arrays A and B of words.  Each word is a string of lowercase letters.
- *
+ * <p>
  * Now, say that word b is a subset of word a if every letter in b occurs in a, including
  * multiplicity.  For example, "wrr" is a subset of "warrior", but is not a subset of "world".
- *
+ * <p>
  * Now say a word a from A is universal if for every b in B, b is a subset of a.
- *
+ * <p>
  * Return a list of all universal words in A.  You can return the words in any order.
- *
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: A = ["amazon","apple","facebook","google","leetcode"], B = ["e","o"]
  * Output: ["facebook","google","leetcode"]
  * Example 2:
- *
+ * <p>
  * Input: A = ["amazon","apple","facebook","google","leetcode"], B = ["l","e"]
  * Output: ["apple","google","leetcode"]
  * Example 3:
- *
+ * <p>
  * Input: A = ["amazon","apple","facebook","google","leetcode"], B = ["e","oo"]
  * Output: ["facebook","google"]
  * Example 4:
- *
+ * <p>
  * Input: A = ["amazon","apple","facebook","google","leetcode"], B = ["lo","eo"]
  * Output: ["google","leetcode"]
  * Example 5:
- *
+ * <p>
  * Input: A = ["amazon","apple","facebook","google","leetcode"], B = ["ec","oc","ceo"]
  * Output: ["facebook","leetcode"]
- *
- *
+ * <p>
+ * <p>
  * Note:
- *
+ * <p>
  * 1 <= A.length, B.length <= 10000
  * 1 <= A[i].length, B[i].length <= 10
  * A[i] and B[i] consist only of lowercase letters.
  * All words in A[i] are unique: there isn't i != j with A[i] == A[j].
  */
-class Solution {
+class Solution2 {
     public List<String> wordSubsets(String[] A, String[] B) {
 
-        List<char[]> splitB = new ArrayList<>(B.length);
-        Set<String> existsB = new HashSet<>();
-        char[] sp = new char[26];
+        int[] splitB = new int[26];
         for (String singleB : B) {
-            Arrays.fill(sp, (char)0);
-            if (existsB.contains(singleB)) {
-                continue;
-            }
-            existsB.add(singleB);
+            int[] tmp = new int[26];
             for (int j = 0; j < singleB.length(); j++) {
-                sp[singleB.charAt(j) - 'a']++;
+                tmp[singleB.charAt(j) - 'a']++;
             }
-            if (existsB.add(new String(sp))) {
-                splitB.add(Arrays.copyOf(sp, 26));
+            for (int i = 0; i < splitB.length; i++) {
+                splitB[i] = Math.max(splitB[i], tmp[i]);
             }
         }
 
+
+
         List<String> result = new ArrayList<>(A.length);
-        Map<String, Boolean> x = new HashMap<>();
-        char[] splitA = new char[26];
         for (String s : A) {
-            Arrays.fill(splitA, (char)0);
+            char[] splitA = new char[26];
             for (int i = 0; i < s.length(); i++) {
                 splitA[s.charAt(i) - 'a']++;
             }
 
-            String splitAString = new String(splitA);
-            if (x.containsKey(splitAString)) {
-                if (x.get(splitAString)) {
-                    result.add(s);
-                }
-                continue;
-            }
-
             boolean isSubset = true;
-            for (int i = 0; i < splitB.size(); i++) {
-                if (!isSubset) {
+            for (int j = 0; j < splitA.length; j++) {
+                if (splitB[j] > splitA[j]) {
+                    isSubset = false;
                     break;
-                }
-                for (int j = 0; j < splitA.length; j++) {
-                    if (splitB.get(i)[j] > splitA[j]) {
-                        isSubset = false;
-                        break;
-                    }
                 }
             }
             if (isSubset) {
-                x.put(splitAString, true);
                 result.add(s);
-            } else {
-                x.put(splitAString, false);
             }
         }
         return result;
