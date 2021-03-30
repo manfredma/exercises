62d0d47510f35833fc45dedc0c1b263417c572ea
maxingfang <maxingfang@meituan.com>
2021-03-30 10:17:05 +0800

    jvm-native

--- jvm/jvm-native/src/main/java/HelloWorld.java
+++ jvm/jvm-native/src/main/java/HelloWorld.java
@@ -2,13 +2,6 @@ public class HelloWorld {
     public native void hello();
 
     static {
-
-        //设置查找路径为当前项目路径
-        String newLib = System.getProperty("path.separator") + "/Users/maxingfang/IdeaProjects/exercises/jvm/jvm-native/";
-        System.setProperty("java.library.path", System.getProperty("java.library.path") + newLib);
-        System.getProperties().forEach((k, v) -> {
-            System.out.println(k + " = " + v);
-        });
         //加载动态库的名称
         System.loadLibrary("hello");
     }
