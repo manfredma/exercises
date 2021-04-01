032299532bbc5ea1c7dcb17623050652f26d29f0
maxingfang <maxingfang@meituan.com>
2021-04-01 14:11:28 +0800

    ddd&variable_hiding

---
+++ java-language/java-basic/src/main/java/manfred/end/java/basic/variable/hiding/Child.java
@@ -0,0 +1,5 @@
+package manfred.end.java.basic.variable.hiding;
+
+class Child extends Parent {
+    String x = "child";
+}
---
+++ java-language/java-basic/src/main/java/manfred/end/java/basic/variable/hiding/Parent.java
@@ -0,0 +1,6 @@
+package manfred.end.java.basic.variable.hiding;
+
+class Parent {
+	String x = "parent";
+}
+
