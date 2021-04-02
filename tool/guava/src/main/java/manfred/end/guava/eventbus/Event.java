60af70d0419b25b3d8e1b94017920d8b9918ecab
maxingfang <maxingfang@meituan.com>
2021-04-02 13:59:03 +0800

    guava-eventbus

--- tool/guava/src/main/java/manfred/end/guava/eventbus/Event.java
+++ tool/guava/src/main/java/manfred/end/guava/eventbus/Event.java
@@ -2,6 +2,6 @@ package manfred.end.guava.eventbus;
 
 class Event {
     public String toString() {
-        return "我是一条消息！" + System.identityHashCode(this);
+        return Thread.currentThread() + ": 我是一条消息！" + System.identityHashCode(this);
     }
 }
--- tool/guava/src/main/java/manfred/end/guava/eventbus/EventBusTest.java
+++ tool/guava/src/main/java/manfred/end/guava/eventbus/EventBusTest.java
@@ -1,21 +1,34 @@
 package manfred.end.guava.eventbus;
 
+import com.google.common.eventbus.AsyncEventBus;
 import com.google.common.eventbus.EventBus;
 import org.junit.Test;
 
-public class EventBusTest {
-    public static void main(String[] args) {
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
-    }
+public class EventBusTest {
 
     /**
-     *
+     * 最简单的 EventBus 实例
      */
     @Test
     public void simpleTest() {
         EventBus eventBus = new EventBus("Test");
         eventBus.register(new Listener());
         eventBus.post(new Event());
+    }
+
+    @Test
+    public void asyncTest() {
+        AsyncEventBus asyncEventBus = new AsyncEventBus(
+                new ThreadPoolExecutor(
+                        2, 3, 30, TimeUnit.SECONDS, new ArrayBlockingQueue<>(30)
+                ));
+        asyncEventBus.register(new Listener());
+        asyncEventBus.post(new Event());
+
     }
 }
 
