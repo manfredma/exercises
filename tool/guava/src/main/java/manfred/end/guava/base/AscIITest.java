ea6ec29dbf3baddc7fdf2d1c8f223b13c73e8837
maxingfang <maxingfang@meituan.com>
2021-04-07 21:21:28 +0800

    guava-study

---
+++ tool/guava/src/main/java/manfred/end/guava/base/AscIITest.java
@@ -0,0 +1,15 @@
+package manfred.end.guava.base;
+
+import com.google.common.base.Ascii;
+import org.junit.Test;
+
+public class AscIITest {
+
+    @Test
+    public void test() {
+        System.out.println(Ascii.toLowerCase("XXX"));
+        System.out.println(Ascii.toUpperCase("xxx"));
+        System.out.println(Ascii.truncate("foobar", 7, "...")); // returns "foobar"
+        System.out.println(Ascii.truncate("foobar", 5, "...")); // returns "fo..."
+    }
+}
