5be24e933a993631a91db166f2a7210d40f1d0ca
maxingfang <maxingfang@meituan.com>
2021-04-07 17:54:02 +0800

    graphql

---
+++ rpc/graphql/src/main/java/manfred/end/graphql/book/BookDetailsApplication.java
@@ -0,0 +1,14 @@
+package manfred.end.graphql.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDetailsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookDetailsApplication.class, args);
+    }
+
+}
+
