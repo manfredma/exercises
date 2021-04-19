441c0d1ff374301cebccff429baaa98a69b10e72
maxingfang <maxingfang@meituan.com>
2021-04-19 10:28:11 +0800

    jvmagent

--- jvm/jvm-agent/src/main/java/manfred/agent/basic/Boot.java
+++ jvm/jvm-agent/src/main/java/manfred/agent/basic/Boot.java
@@ -1,17 +1,23 @@
 package manfred.agent.basic;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class Boot {
-    public static void main(String[] args) {
-        new Throwable().printStackTrace(System.out);
+    public static void main(String[] args) throws Exception {
+        // new Throwable().printStackTrace(System.out);
         System.out.println("hello, world!");
+        loadAndExeX();
+        loadAndExeX();
+    }
 
-        classLoader = new DiskClassLoader("jvm/jvm-classloader/clazz/");
-        Class<?> c = classLoader.loadClass("manfred.end.clazz.loader.X");
-        System.out.println("\t" + c + " 的类加载器是 " + c.getClassLoader());
-        Object o = c.newInstance();
-        Method m = c.getMethod("sayHello");
-        m.invoke(o);
+    private static void loadAndExeX() throws ClassNotFoundException, InstantiationException,
+            IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        ClassLoader classLoader2 = new DiskClassLoader("clazz/");
+        Class<?> c2 = classLoader2.loadClass("manfred.end.clazz.loader.X");
+        System.out.println("\t" + c2 + " 的类加载器是 " + c2.getClassLoader());
+        Object o2 = c2.newInstance();
+        Method m2 = c2.getMethod("sayHello");
+        m2.invoke(o2);
     }
 }
