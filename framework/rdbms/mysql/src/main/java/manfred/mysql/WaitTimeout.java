a738ce352584d61b398ad493c59f317114208466
maxingfang <maxingfang@meituan.com>
2021-06-07 21:04:04 +0800

    DDD 图等 mysql 服务端超时

---
+++ framework/rdbms/mysql/src/main/java/manfred/mysql/WaitTimeout.java
@@ -0,0 +1,22 @@
+package manfred.mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class WaitTimeout {
+    public static void main(String[] args) throws ClassNotFoundException, InterruptedException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/manfred", "coupon", "coupon");
+        System.out.println("Database Connection Established.\n");
+        System.out.println(connection.createStatement().execute("set session wait_timeout=2"));
+
+        Thread.sleep(3000);
+        ResultSet rs = connection.createStatement().executeQuery("select now() as date_str ");
+        while (rs.next()) {
+            System.out.println(rs.getTimestamp("date_str"));
+        }
+
+    }
+}
