707693580ff9c92f8f7b8fecbe0b4d070733d448
maxingfang <maxingfang@meituan.com>
2021-07-05 15:11:32 +0800

    fix

--- jvm/jvm-memory/src/main/java/heap/oom/HeapOom.java
+++ jvm/jvm-memory/src/main/java/heap/oom/HeapOom.java
@@ -2,12 +2,15 @@ package heap.oom;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class HeapOom {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         List<byte[]> x = new ArrayList<>();
-        while (true) {
-            x.add(new byte[1024 * 1024 * 1024]);
+        for (int i = 0; i < 100; i++) {
+            // TimeUnit.MILLISECONDS.sleep(100);
+            x.add(new byte[1024 * 1024]);
         }
+        System.in.read();
     }
 }
