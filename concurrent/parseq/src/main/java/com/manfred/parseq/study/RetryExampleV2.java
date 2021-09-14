046408027fd82b792ecde5ef73548b585d74b47e
maxingfang <maxingfang@meituan.com>
2021-09-14 14:11:19 +0800

    parseq retry

--- concurrent/parseq/src/main/java/com/manfred/parseq/study/RetryExampleV2.java
+++ concurrent/parseq/src/main/java/com/manfred/parseq/study/RetryExampleV2.java
@@ -3,6 +3,7 @@ package com.manfred.parseq.study;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.linkedin.parseq.Engine;
 import com.linkedin.parseq.EngineBuilder;
+import com.linkedin.parseq.ParTask;
 import com.linkedin.parseq.Task;
 import com.linkedin.parseq.promise.Promises;
 import com.linkedin.parseq.promise.SettablePromise;
@@ -15,69 +16,54 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.*;
 import java.util.function.Function;
 
-public class RetryExample {
+public class RetryExampleV2 {
 
     private static List<Task> taskList = new ArrayList<>();
 
-    private static ExecutorService poolExecutor =
+    private static ExecutorService outPool =
             new ThreadPoolExecutor(20, 50, 0L, TimeUnit.MILLISECONDS, new SynchronousQueue<>(),
                     new ThreadFactoryBuilder().setDaemon(false)
                             .setNameFormat("out-%d")
                             .build(), new AsyncCallerRunsPolicy());
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(RetryExample.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(RetryExampleV2.class);
 
+    private static int x = 1;
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        ExecutorService poolExecutor =
+        ExecutorService enginePool =
                 new ThreadPoolExecutor(20, 50, 0L, TimeUnit.MILLISECONDS, new SynchronousQueue<>(),
                         new ThreadFactoryBuilder().setDaemon(false)
                                 .setNameFormat("parseq-%d")
                                 .build(), new AsyncCallerRunsPolicy());
 
-        Engine engine = new EngineBuilder().setTaskExecutor(poolExecutor)
+        Engine engine = new EngineBuilder().setTaskExecutor(enginePool)
                 .setTimerScheduler(Executors.newScheduledThreadPool(2))
                 .build();
-        Task task = createCompoundTask();
+        Task task = createRetryTask();
         engine.run(task, "xxx");
         task.await();
         engine.shutdown();
         System.exit(0);
     }
 
-    static int success = 1;
-
-    private static Task<Object> createTask(int id) {
-        Task<Object> task = Task.async("Task " + id, () -> {
+    private static Task<Object> createAsyncTask(String desc, boolean isException) {
+        Task<Object> task = Task.async("Task " + desc, () -> {
             SettablePromise<Object> promise = Promises.settable();
-            poolExecutor.execute(() -> {
-                try {
-                    Thread.sleep(id + 200L);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                LOGGER.info("id = {} 开始在外部线程池执行", id);
-
-                if (!(success % 2 == 0) && id == 20) {
-                    // LOGGER.info("id = {} 执行失败", id);
-                    success++;
-                    Exception exception = new RuntimeException();
-                    promise.fail(exception);
-                } else if (id == 20) {
-                    // LOGGER.info("id = {} 执行成功", id);
-                    success++;
-                    promise.done("Task " + id + " result!!!");
+            outPool.execute(() -> {
+                LOGGER.info("id = {} 开始在外部线程池执行", desc);
+                if (isException) {
+                    promise.fail(new RuntimeException(desc));
                 } else {
-                    // LOGGER.info("id = {} 执行成功", id);
-                    promise.done("Task " + id + " result!!!");
+                    promise.done("Task " + desc + " result!!!");
                 }
-                LOGGER.info("id = {} 执行完成", id);
-                // return null;
+                LOGGER.info("id = {} 执行完成", desc);
             });
             return promise;
         });
@@ -85,29 +71,28 @@ public class RetryExample {
         return task;
     }
 
-    private static Task createCompoundTask() {
+    private static Task createRetryTask() {
         Function<Throwable, ErrorClassification> errorClassifier =
                 error -> error instanceof Exception ? ErrorClassification.RECOVERABLE
                         : ErrorClassification.UNRECOVERABLE;
 
         RetryPolicy retryPolicy = new RetryPolicyBuilder()
-                .setTerminationPolicy(TerminationPolicy.limitAttempts(2))
-                // .setBackoffPolicy()
+                .setTerminationPolicy(TerminationPolicy.limitAttempts(20))
                 .setErrorClassifier(errorClassifier)
                 .build();
 
-        Task re = Task.withRetryPolicy(retryPolicy, () -> {
-            Task<Object> result = createTask(1);
+        Task<Object> re = Task.withRetryPolicy(retryPolicy, () -> {
+            Task<Object> result = createAsyncTask("retry-" + x++ + "", true);
             return result;
         }).map("map-print-1", (x) -> {
             LOGGER.info("输出一下结果, {}", x);
             return x;
         });
-        Task x2 = createTask(0).map("map-print-2", (x) -> {
+        Task<Object> x2 = createAsyncTask("0", false).map("map-print-2", (x) -> {
             LOGGER.info("输出一下结果, {}", x);
             return x;
         });
-        Task task = Task.par(re, x2);
+        ParTask<Object> task = Task.par(Arrays.asList(re, x2));
         taskList.add(task);
         return task;
     }
