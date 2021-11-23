44fe1b52b71b06ba5bd85b3200a57a25e368d9f2
maxingfang <maxingfang@meituan.com>
2021-11-23 21:33:37 +0800

    商流

--- tool/piranha/src/test/java/com/uber/mylib/MyClassTest.java
+++ tool/piranha/src/test/java/com/uber/mylib/MyClassTest.java
@@ -1,13 +1,17 @@
 package com.uber.mylib;
 
+import org.junit.Test;
+
 public class MyClassTest {
 
     private XPTest expt;
 
+    @Test
     public void x() {
         expt.enableFlag(TestExperimentName.SAMPLE_STALE_FLAG);
     }
 
+    @Test
     public void y() {
         expt.disableFlag(TestExperimentName.SAMPLE_STALE_FLAG);
     }
