9cd7be0f5806e044c40d269b603ca30b6f64eef7
maxingfang <maxingfang@meituan.com>
2022-06-02 14:25:12 +0800

    es init

---
+++ search/es/es-client/src/main/java/manfred/end/es/client/InitDemo.java
@@ -0,0 +1,21 @@
+package manfred.end.es.client;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+
+/**
+ * @Description: 获取Java High Level REST Client客户端
+ * @date 2018年6月23日
+ */
+public class InitDemo {
+
+    public static RestHighLevelClient getClient() {
+
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"),
+                        new HttpHost("localhost", 9201, "http")));
+
+        return client;
+    }
+}
