8f957b3e6625df90885a95b86dc22309e9450373
maxingfang <maxingfang@meituan.com>
2022-06-02 15:09:52 +0800

    jackson init

--- java-language/java-serialization/json/src/main/java/jackson/Boot.java
+++ java-language/java-serialization/json/src/main/java/jackson/Boot.java
@@ -2,7 +2,6 @@ package jackson;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import model.Customer;
-import model.Order;
 
 /**
  * @author manfred on 2022/5/23.
@@ -11,9 +10,6 @@ public class Boot {
     public static void main(String[] args) throws Exception {
 
         Customer customer = new Customer("manfred", 100);
-        Order order = new Order();
-
-        order.setCustomer(customer);
 
         ObjectMapper objectMapper = new ObjectMapper();
         System.out.println(objectMapper.writeValueAsString(customer));
