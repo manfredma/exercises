61544f60657e6aef74b139749e2039c3d98d1a48
maxingfang <maxingfang@meituan.com>
2022-06-02 14:53:09 +0800

    jackson init

---
+++ java-language/java-serialization/json/src/main/java/model/Order.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Order {
+
+    private Customer customer;
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+}
