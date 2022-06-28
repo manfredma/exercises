a94d364e7d89196e0b657b1915035e85979e0bb0
maxingfang <maxingfang@meituan.com>
2022-06-28 14:28:39 +0800

    double toString

---
+++ java-language/java-basic/src/main/java/manfred/end/java/basic/Boot.java
@@ -0,0 +1,15 @@
+package manfred.end.java.basic;
+
+public class Boot {
+    public static void main(String[] args) {
+        System.out.println(0.01);
+        System.out.println(0.02);
+        System.out.println(0.03);
+        System.out.println(0.04);
+        System.out.println(0.05);
+        System.out.println(0.06);
+        System.out.println(0.07);
+        System.out.println(0.08);
+        System.out.println(0.090000000000000000000006);
+    }
+}
