79b08d6d6b997e635852f7d163f58b0d681bb0d9
maxingfang <maxingfang@meituan.com>
2022-07-04 15:49:17 +0800

    double toString

--- framework/log/log4j2/src/main/java/com/foo/MyApp.java
+++ framework/log/log4j2/src/main/java/com/foo/MyApp.java
@@ -6,18 +6,16 @@ import org.slf4j.MDC;
 public class MyApp {
 
     private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(MyApp.class);
-    private static final org.slf4j.Logger LOGGERV2 = LoggerFactory.getILoggerFactory().getLogger(MyApp.class.getName());
-    private static final org.slf4j.Logger LOGGERV3 = LoggerFactory.getLogger(MyApp.class);
-    private static final org.slf4j.Logger LOGGERV4 = LoggerFactory.getILoggerFactory().getLogger(MyApp.class.getName());
 
     public static void main(final String... args) {
 
-        MDC.put("transaction.owner", "MDC-owner");
+//        MDC.put("apiVersion", "${${ctx:apiVersion}}");
+//        MDC.put("transaction.owner", "MDC-owner");
         LOGGER.info("Entering application.");
-        Bar bar = new Bar();
-        if (!bar.doIt()) {
-            LOGGER.error("Didn't do it.");
-        }
-        LOGGER.info("Exiting application.");
+//        Bar bar = new Bar();
+//        if (!bar.doIt()) {
+//            LOGGER.error("Didn't do it.");
+//        }
+//        LOGGER.info("Exiting application.");
     }
 }
