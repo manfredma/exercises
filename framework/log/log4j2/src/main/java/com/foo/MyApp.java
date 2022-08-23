770132485de81fbc605f7aa8ec33567618a215ae
maxingfang <maxingfang@meituan.com>
2022-08-23 11:25:34 +0800

    log4j2 example

--- framework/log/log4j2/src/main/java/com/foo/MyApp.java
+++ framework/log/log4j2/src/main/java/com/foo/MyApp.java
@@ -1,5 +1,6 @@
 package com.foo;
 
+import java.util.concurrent.Executors;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
@@ -8,10 +9,14 @@ public class MyApp {
     private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(MyApp.class);
 
     public static void main(final String... args) {
-
+        Exception e = new Exception();
 //        MDC.put("apiVersion", "${${ctx:apiVersion}}");
 //        MDC.put("transaction.owner", "MDC-owner");
-        LOGGER.info("Entering application.");
+        LOGGER.info("Entering application.", e);
+
+        Executors.newSingleThreadExecutor().submit(() -> {
+            LOGGER.info("xxxx", e);
+        });
 //        Bar bar = new Bar();
 //        if (!bar.doIt()) {
 //            LOGGER.error("Didn't do it.");
