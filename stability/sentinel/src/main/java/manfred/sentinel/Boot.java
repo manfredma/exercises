ca9071067fc4405a1e832239c069aca54e41b3f2
maxingfang <maxingfang@meituan.com>
2022-08-23 11:25:03 +0800

    sentinel example

--- stability/sentinel/src/main/java/manfred/sentinel/Boot.java
+++ stability/sentinel/src/main/java/manfred/sentinel/Boot.java
@@ -6,27 +6,31 @@ import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.alibaba.csp.sentinel.slots.block.RuleConstant;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author manfred
  * @since 2019-12-17 上午9:58
  */
 public class Boot {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // 配置规则.
         initFlowRules();
+
+        AtomicInteger index = new AtomicInteger(0);
 
         while (true) {
             // 1.5.0 版本开始可以直接利用 try-with-resources 特性，自动 exit entry
             try (Entry entry = SphU.entry("HelloWorld")) {
                 // 被保护的逻辑
-                System.out.println("hello world");
+                System.out.println("hello world - " + index.addAndGet(1));
             } catch (BlockException ex) {
                 // 处理被流控的逻辑
                 System.out.println("blocked!");
+                TimeUnit.MILLISECONDS.sleep(100L);
             }
         }
     }
