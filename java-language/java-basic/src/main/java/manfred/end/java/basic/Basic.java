0199ac90f4c66d6498a13764da98b4f7a661a28e
maxingfang <maxingfang@meituan.com>
2022-09-28 14:36:24 +0800

    basic

--- java-language/java-basic/src/main/java/manfred/end/java/basic/Basic.java
+++ java-language/java-basic/src/main/java/manfred/end/java/basic/Basic.java
@@ -2,6 +2,10 @@ package manfred.end.java.basic;
 
 public class Basic {
     public static void main(String[] args) {
+        System.out.println(null + "");
+        System.out.println((String) null);
+        String s = null + "";
+        System.out.println(s.length());
 
     }
 }
