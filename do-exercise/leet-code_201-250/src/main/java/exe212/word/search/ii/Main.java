498cc2299a36ba50daf7d101e414c2881be692c4
maxingfang <maxingfang@meituan.com>
2023-02-14 17:08:57 +0800

    leetcode 212

--- do-exercise/leet-code_201-250/src/main/java/exe212/word/search/ii/Main.java
+++ do-exercise/leet-code_201-250/src/main/java/exe212/word/search/ii/Main.java
@@ -30,7 +30,32 @@ The values of words are distinct.
 package exe212.word.search.ii;
 
 /**
- * @author Manfred since 2019/9/29
+ *
  */
 public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.findWords(new char[][]{
+                        {'o', 'a', 'b', 'n'},
+                        {'o', 't', 'a', 'e'},
+                        {'a', 'h', 'k', 'r'},
+                        {'a', 'f', 'l', 'v'},
+                },
+                new String[]{"oa", "oaa"}));
+
+        System.out.println(solution.findWords(new char[][]{
+                        {'o', 'a', 'a', 'n'},
+                        {'e', 't', 'a', 'e'},
+                        {'i', 'h', 'k', 'r'},
+                        {'i', 'f', 'l', 'v'}
+                },
+                new String[]{"oath", "pea", "eat", "rain"}));
+
+        System.out.println(solution.findWords(new char[][]{
+                        {'a', 'b'},
+                        {'c', 'd'}
+                },
+                new String[]{"abcb"}));
+
+    }
 }
