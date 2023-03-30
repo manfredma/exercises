13d7c9f66b6a6d51ee7a541232302d813c29a89a
maxingfang <maxingfang@meituan.com>
2023-03-30 17:07:18 +0800

    init

--- concurrent/thread-local/src/main/java/manfred/end/thread/local/Boot.java
+++ concurrent/thread-local/src/main/java/manfred/end/thread/local/Boot.java
@@ -1,14 +1,31 @@
 package manfred.end.thread.local;
 
+import java.lang.ref.WeakReference;
+import java.lang.reflect.Field;
+
 /**
  * @author manfred on 2021/5/9.
  */
 public class Boot {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
         ThreadLocal<String> threadLocal = new ThreadLocal<>();
         threadLocal.set("xxx");
-        System.out.println(threadLocal.get());
 
-        System.out.println(ThreadLocalHolder.x.get());
+        Field field = Thread.class.getDeclaredField("threadLocals");
+        field.setAccessible(true);
+        Object map = field.get(Thread.currentThread());
+        Class m = map.getClass();
+        Field t = m.getDeclaredField("table");
+        t.setAccessible(true);
+        WeakReference[] o = (WeakReference[]) t.get(map);
+        for (WeakReference o1 : o) {
+            if (o1 != null) {
+                Class ec = o1.getClass();
+                Field ecF = ec.getDeclaredField("value");
+                ecF.setAccessible(true);
+                System.out.println("value: " + ecF.get(o1));
+            }
+        }
+
     }
 }
