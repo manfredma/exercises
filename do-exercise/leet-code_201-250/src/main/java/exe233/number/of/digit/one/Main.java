c86ddc26138facd347a59111fdd873da0814f8bf
maxingfang <maxingfang@meituan.com>
2023-06-07 19:02:25 +0800

    leetcode 233

--- do-exercise/leet-code_201-250/src/main/java/exe233/number/of/digit/one/Main.java
+++ do-exercise/leet-code_201-250/src/main/java/exe233/number/of/digit/one/Main.java
@@ -10,5 +10,11 @@ public class Main {
         System.out.println(solution.countDigitOne(0));
         System.out.println(solution.countDigitOne(12));
         System.out.println(solution.countDigitOne(20));
+
+        SolutionV2 solutionV2 = new SolutionV2();
+        System.out.println(solutionV2.countDigitOne(13));
+        System.out.println(solutionV2.countDigitOne(0));
+        System.out.println(solutionV2.countDigitOne(12));
+        System.out.println(solutionV2.countDigitOne(20));
     }
 }
