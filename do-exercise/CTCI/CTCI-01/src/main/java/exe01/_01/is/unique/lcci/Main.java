73576eed92b0d4209990d8f4da50ef1f57faeb4e
maxingfang <maxingfang@meituan.com>
2023-06-17 11:06:59 +0800

    CTCI 01 (1-2)

---
+++ do-exercise/CTCI/CTCI-01/src/main/java/exe01/_01/is/unique/lcci/Main.java
@@ -0,0 +1,9 @@
+package exe01._01.is.unique.lcci;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isUnique("leetcode"));
+        System.out.println(solution.isUnique("abc"));
+    }
+}
