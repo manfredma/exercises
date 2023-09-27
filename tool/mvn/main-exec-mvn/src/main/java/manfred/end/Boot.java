fcd7c10086236372a8cda880b53c37f7c3341f2c
maxingfang <maxingfang@meituan.com>
2023-09-27 17:41:30 +0800

    ...

---
+++ tool/mvn/main-exec-mvn/src/main/java/manfred/end/Boot.java
@@ -0,0 +1,7 @@
+package manfred.end;
+
+public class Boot {
+    public static void main(String[] args) {
+        System.out.println("Hello, Main!");
+    }
+}
---
+++ tool/open-rewrite/src/main/java/org/openrewrite/java/RunRewriteManually.java
@@ -0,0 +1,63 @@
+package org.openrewrite.java;
+
+import static java.util.Collections.emptyList;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.Recipe;
+import org.openrewrite.Result;
+import org.openrewrite.SourceFile;
+import org.openrewrite.config.Environment;
+import org.openrewrite.internal.InMemoryLargeSourceSet;
+
+public class RunRewriteManually {
+    public static void main(String[] args) throws IOException {
+        // determine your project directory and provide a list of
+        // paths to jars that represent the project's classpath
+        Path projectDir = Paths.get(".");
+        List<Path> classpath = emptyList();
+
+
+        // put any rewrite recipe jars on this main method's runtime classpath
+        // and either construct the recipe directly or via an Environment
+        Environment environment = Environment.builder().scanRuntimeClasspath().build();
+        Recipe recipe = new UseStaticImport("java.util.List remove(..)");
+
+        // create a JavaParser instance with your classpath
+        JavaParser javaParser = JavaParser.fromJavaVersion()
+                .classpath(classpath)
+                .build();
+
+        // walk the directory structure where your Java sources are located
+        // and create a list of them
+        List<Path> sourcePaths = Files.find(projectDir, 999, (p, bfa) ->
+                        bfa.isRegularFile() && p.getFileName().toString().endsWith(".java"))
+                .collect(Collectors.toList());
+
+        ExecutionContext ctx = new InMemoryExecutionContext(Throwable::printStackTrace);
+
+        // parser the source files into LSTs
+        List<SourceFile> cus = javaParser.parse(sourcePaths, projectDir, ctx)
+                .collect(Collectors.toList());
+
+        // collect results
+        List<Result> results = recipe.run(new InMemoryLargeSourceSet(cus), ctx)
+                .getChangeset().getAllResults();
+
+        for (Result result : results) {
+            // print diffs to the console
+            System.out.println(result.diff(projectDir));
+
+            // or overwrite the file on disk with changes.
+            // Files.writeString(result.getAfter().getSourcePath(),
+            //        result.getAfter().printAll());
+        }
+
+    }
+}
