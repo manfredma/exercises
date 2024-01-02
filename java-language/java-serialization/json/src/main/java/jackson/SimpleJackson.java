37901bf69842ca7a9203423cad69e45296a6fe22
maxingfang <maxingfang@meituan.com>
2024-01-02 14:51:51 +0800

    jackson polish

--- java-language/java-serialization/json/src/main/java/jackson/SimpleJackson.java
+++ java-language/java-serialization/json/src/main/java/jackson/SimpleJackson.java
@@ -1,6 +1,8 @@
 package jackson;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import model.Customer;
 
 /**
@@ -9,9 +11,14 @@ import model.Customer;
 public class SimpleJackson {
     public static void main(String[] args) throws Exception {
 
-        Customer customer = new Customer("manfred", 100);
-
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        String nullJson = objectMapper.writeValueAsString(null);
+        if (nullJson != null && !nullJson.isEmpty()) {
+            System.out.println("output: " + nullJson);
+        }
+
+        Customer customer = new Customer("manfred", 100);
         System.out.println(objectMapper.writeValueAsString(customer));
 
         System.out.println(objectMapper.readValue("null", String.class));
